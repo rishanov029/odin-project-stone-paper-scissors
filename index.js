8f0e63bf17a8955b3b53e8745a5e0554fa2a5627
rishanov029 <rishanov029@gmail.com>
2026-03-06 19:01:41 +0530

    first commit

--- index.js
+++ index.js
@@ -1,53 +1,80 @@
-const choices=["rock","paper","scissors"];
-const playerDisplay=document.getElementById("playerDisplay");
-const computerDisplay=document.getElementById("computerDisplay");
-const resultDisplay=document.getElementById("resultDisplay");
-const playerscoreDisplay=document.getElementById("playerscoreDisplay");
-const computerscoreDisplay=document.getElementById("computerscoreDisplay");
-let playerScore=0;
-let computerScore=0;
+const choices = ["rock","paper","scissors"];
 
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerscoreDisplay = document.getElementById("playerscoreDisplay");
+const computerscoreDisplay = document.getElementById("computerscoreDisplay");
 
+let playerScore = 0;
+let computerScore = 0;
+
+const winScore = 5; 
 
 function playGame(playerChoice){
 
-    const computerChoice=choices[Math.floor(Math.random()*3)];
-    let result="";
-    if (playerChoice===computerChoice){
-        result="ohohh,IT'S A TIE!!!!";
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    let result = "";
+
+    if(playerChoice === computerChoice){
+        result = "IT'S A TIE!";
     }
     else{
         switch(playerChoice){
-            case"rock":
-            result=(computerChoice==="scissors") ? "yehhh buddy, YOU WIN!!!" : "ohoo no!!!, YOU LOSE!!!";
-            break;
-            case"paper":
-            result=(computerChoice==="rock") ? "yehhh buddy, YOU WIN!!!" : "ohoo no!!!, YOU LOSE!!!";
-            break;
-            case"scissors":
-            result=(computerChoice==="paper") ? "yehhh buddy, YOU WIN!!!" : "ohoo no!!!, YOU LOSE!!!";
+
+            case "rock":
+            result = (computerChoice === "scissors") ?
+            "YEHAA BUDDY, YOU WIN!" : "OHOO NO!!, YOU LOSE!";
             break;
 
-    }
+            case "paper":
+            result = (computerChoice === "rock") ?
+            "YEHAA BUDDY, YOU WIN!" : "OHOO NO!!, YOU LOSE!";
+            break;
+
+            case "scissors":
+            result = (computerChoice === "paper") ?
+            "YEHAA BUDDY, YOU WIN!" : "OHOO NO!!, YOU LOSE!";
+            break;
+        }
     }
 
-    playerDisplay.textContent =`PLAYER: ${playerChoice}`;
-    computerDisplay.textContent =`computer: ${computerChoice}`;
-    resultDisplay.textContent =result;
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent = result;
 
     resultDisplay.classList.remove("greenText","redText");
-    
-    switch(result){
-        case "yehhh buddy, YOU WIN!!!":
+
+    if(result === "YEHAA BUDDY, YOU WIN!"){
         resultDisplay.classList.add("greenText");
         playerScore++;
-        playerscoreDisplay.textContent=playerScore;
-        break;  
-        case "ohoo no!!!, YOU LOSE!!!":
-        resultDisplay.classList.add("redText");
-        computerScore++;
-        computerscoreDisplay.textContent=computerScore;
-        break;
+        playerscoreDisplay.textContent = playerScore;
     }
 
+    else if(result === "OHOO NO!!, YOU LOSE!"){
+        resultDisplay.classList.add("redText");
+        computerScore++;
+        computerscoreDisplay.textContent = computerScore;
+    }
+
+    
+ if(playerScore ===winScore){
+    alert("🥂🎊 CONGRATULATIONS! YOU WON THE GAME!");
+    resetGame();
+ }
+ 
+    if(computerScore ===winScore){
+        alert("😞💻 COMPUTER WINS! BETTER LUCK NEXT TIME!");
+        resetGame();
+    }
 }
+
+
+ function resetGame(){
+    playerScore=0;
+    computerScore=0;
+
+    playerscoreDisplay.textContent = playerScore;
+    computerscoreDisplay.textContent = computerScore;
+    resultDisplay.textContent = "";
+ }
